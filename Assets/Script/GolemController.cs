fca5e8a06d231a5f4d36d6d52a845cc6d40ebf21
JoeASB <joe.sb.uc@gmail.com>
2019-09-08 02:23:06 -0500

    Basic movement and attack of the fire boss golem

---
+++ Assets/Script/GolemController.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GolemController : MonoBehaviour
+{
+	[SerializeField]
+	private Animator animator;
+	[SerializeField]
+	private float walkSpeed;
+	[SerializeField]
+	private float maxCloseDistance;
+	[SerializeField]
+	private float attackCooldownTime;
+	private bool canAttack = true;
+
+	private readonly string PLAYER_TAG = "Player";
+	private GameObject player;
+
+	private bool isDead = false;
+	private bool animDeadPlayed= false;
+
+	#region
+	private readonly string IDLE_STATE_NAME = "Idle1";
+	private readonly string WALK_STATE_NAME = "Walk";
+	private readonly string DEATH_STATE_NAME = "Death1";
+	private readonly string GET_HIT_STATE_NAME = "GetHit2";
+	private readonly string NORMAL_ATTACK_STATE_NAME = "Attack3";
+	private readonly string FIRE_ATTACK_STATE_NAME = "Attack2";
+	#endregion
+
+	#region Animation Parameters
+	private readonly string DEATH_TRIGGER = "Death1";
+	private readonly string FIRE_ATTACK_TRIGGER = "Attack2";
+	private readonly string ATTACK_TRIGGER = "Attack3";
+	private readonly string GET_HIT_TRIGGER = "Attack3";
+	private readonly string WALKING_BOOL = "Walking";
+	#endregion
+	
+	void Start()
+    {
+		player = GameObject.FindGameObjectWithTag(PLAYER_TAG);
+    }
+	
+    void Update()
+    {
+
+		if(Input.GetKeyDown(KeyCode.M)) 
+		{
+			isDead = true;
+		}
+
+		if(!isDead) 
+		{
+			transform.LookAt(new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z));
+			Vector3 playerPos = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z);
+			if(Vector3.Distance(playerPos, transform.position) > maxCloseDistance && (animator.GetCurrentAnimatorStateInfo(0).IsName(IDLE_STATE_NAME) || animator.GetCurrentAnimatorStateInfo(0).IsName(WALK_STATE_NAME))) //Check if the distances between the object are bigger than the max close distance
+			{
+				transform.position = Vector3.MoveTowards(transform.position, playerPos, walkSpeed * Time.deltaTime);
+				Walk(true);
+			} else //If the boss are the enough closer, can attack the enemy
+			{
+				Walk(false);
+				if(canAttack) 
+				{
+					canAttack = false;
+					Attack();
+					StartCoroutine(CooldownAttack());
+				}
+			}
+		} else 
+		{
+			if(!animDeadPlayed) 
+			{
+				Die();
+				animDeadPlayed = true;
+			}
+		}
+    }
+
+	public void Attack() 
+	{
+		Walk(false);
+		animator.SetTrigger(ATTACK_TRIGGER);
+	}
+
+	public void AttackWithFire() 
+	{
+		Walk(false);
+		animator.SetTrigger(FIRE_ATTACK_TRIGGER);
+	}
+
+	public void Walk(bool isWalking) 
+	{
+		animator.SetBool(WALKING_BOOL, isWalking);
+	}
+
+	public void Die() 
+	{
+		Walk(false);
+		animator.SetTrigger(DEATH_TRIGGER);
+	}
+
+	private IEnumerator CooldownAttack ()
+	{
+		yield return new WaitForSeconds(attackCooldownTime);
+		canAttack = true;
+	}
+}
