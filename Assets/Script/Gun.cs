054b417c46fb726176acce7e5fb7dbd472e69c49
Ed <eduardo_casillas@hotmail.com>
2019-09-22 01:04:16 -0500

    Continue adding Gun functionality.

--- Assets/Script/Gun.cs
+++ Assets/Script/Gun.cs
@@ -2,17 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Gun : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class Gun : MonoBehaviour {
+	public ParticleSystem Particles;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private void Update() {
+		var fire = Input.GetAxis("Fire1");
+		if (fire > 0f) {
+			//if (!Particles.isPlaying) 
+				Particles.Play();
+		} else {
+			//if(Particles.isPlaying)
+				Particles.Stop();
+		}
+	}
+
 }
--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,11 +1,17 @@
 ﻿using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
+	public GameObject Gun;
+
+	private void Awake() {
+		Gun.SetActive(false);
+	}
+
 	private void OnTriggerEnter(Collider other) {
 		var pickup = other.gameObject.GetComponent<Pickup>();
 		if (pickup) {
 			Debug.Log("Pickup was taken");
+			Gun.SetActive(true);
 			Destroy(pickup.gameObject);
 		}
 	}
