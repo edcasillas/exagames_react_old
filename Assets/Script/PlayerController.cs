6cb02c359b4f42cd72abdfaf1d9d7575a141e6fd
Ed <eduardo_casillas@hotmail.com>
2019-09-23 09:48:02 -0500

    Created health for the player

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -2,9 +2,13 @@
 
 public class PlayerController : MonoBehaviour {
 	public GameObject Gun;
+	public int InitialHealth;
+
+	public int Health { get; private set; }
 
 	private void Awake() {
 		Gun.SetActive(false);
+		Health = InitialHealth;
 	}
 
 	private void OnTriggerEnter(Collider other) {
@@ -13,6 +17,13 @@ public class PlayerController : MonoBehaviour {
 			Debug.Log("Pickup was taken");
 			Gun.SetActive(true);
 			Destroy(pickup.gameObject);
+		}
+	}
+
+	public void TakeDamage(int damage) {
+		Health -= damage;
+		if (Health <= 0) {
+			// is dead!
 		}
 	}
 }
