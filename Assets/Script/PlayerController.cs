b4a40724de2323fd42b59334f47b11b9921f7efb
Nick <nick@blayzegames.com>
2019-10-09 15:04:02 -0300

    Start adding logic for the player health

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -16,9 +16,25 @@ public class PlayerController : CharacterThirdPerson {
 
 	private GameObject equippedWeapon;
 
+	public int InitialHealth;
+
+	public int Health { get; private set; }
+
+	private void Awake() {
+		Health = InitialHealth;
+	}
+
 	protected virtual void Update() {
 		base.Update();
 		HandleAiming();
+	}
+
+	public void TakeDamage(int damage) {
+		Health -= damage;
+
+		if (Health <= 0) {
+			// is dead!
+		}
 	}
 
 	private void HandleAiming() {
@@ -44,5 +60,19 @@ public class PlayerController : CharacterThirdPerson {
 	public void OnWeaponPickedUp(GameObject weapon) {
 		equippedWeapon = weapon;
 		AimController.ik.solver.transform = weapon.transform.parent;
+	}
+
+	private void OnCollisionEnter(Collision collision) {
+		CheckIfShouldReceiveDamage(collision.gameObject);
+	}
+
+	private void OnParticleCollision(GameObject other) {
+		CheckIfShouldReceiveDamage(other);
+	}
+
+	private void CheckIfShouldReceiveDamage(GameObject collider) {
+		var col = collider.GetComponent<PlayerDamagerObject>();
+		if (col)
+			TakeDamage(col.DamageToProvoke);
 	}
 }
