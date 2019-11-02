88dcad48af7cf007600f1594a671557b89b7a415
Enrique Lara <brlarach@ucq.edu.mx>
2019-11-02 17:28:57 -0600

    Added player sound: walking, running, jumping(not working as expected), taking damage

---
+++ Assets/Sounds/PlayerSounds.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSounds : MonoBehaviour
+{
+	[SerializeField]
+	private AudioSource aS;
+	[SerializeField]
+	private AudioClip[] playerRockFootsteps;
+	[SerializeField]
+	private AudioClip[] playerRockWalkFootsteps;
+	[SerializeField]
+	private AudioClip takingDamage;
+	[SerializeField]
+	private AudioClip jump;
+
+	private void Start() {
+		aS = GetComponent<AudioSource>();
+		if (!aS) {
+			Debug.LogError("PlayerHasNoAudioSource");
+		}
+	}
+
+	public void PlayPlayerStepSoundRock() 
+	{
+		int num = Random.Range(0, playerRockFootsteps.Length);
+		aS.PlayOneShot(playerRockFootsteps[num]);
+	}
+	public void PlayPlayerWalkStepSoundRock() {
+		int num = Random.Range(0, playerRockWalkFootsteps.Length);
+		aS.PlayOneShot(playerRockWalkFootsteps[num]);
+	}
+	public void PlayTakingDamageSound() {
+		aS.PlayOneShot(takingDamage);
+	}
+	public void PlayJumpSound() {
+		aS.PlayOneShot(jump);
+	}
+}
