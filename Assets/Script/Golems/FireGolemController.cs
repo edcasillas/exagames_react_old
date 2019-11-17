54918f86a1cd0cb87655a682a61db1487917fefa
JoeASB <joe.sb.uc@gmail.com>
2019-11-17 14:26:18 -0600

    Added references of the golem in the volcano level

--- Assets/Script/Golems/FireGolemController.cs
+++ Assets/Script/Golems/FireGolemController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FireGolemController : MonoBehaviour
+public class FireGolemController : _GolemController
 {
     // Start is called before the first frame update
     void Start()
