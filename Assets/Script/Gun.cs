31361ba29cbd55fd2f94faf25e307af76a8f15e6
Enrique Lara <brlarach@ucq.edu.mx>
2019-11-17 01:41:21 -0600

    Added gun Shooting and reloading sounds

--- Assets/Script/Gun.cs
+++ Assets/Script/Gun.cs
@@ -10,13 +10,25 @@ public class Gun : MonoBehaviour {
 
 	private bool isRecovering;
 
+	[Header("Sound Variables")]
+	[SerializeField] GunSounds gS;
+
 	private void Update() {
 		if (isRecovering) {
+
+			
+			if(!gS.GetAudioSource().isPlaying) {
+
+			gS.PlayReloadingSound();
+			}
+			
 			Particles.Stop();
 			WaterLevel += ConsumeSpeed * 2 * Time.deltaTime;
 			if (WaterLevel >= 100) {
 				WaterLevel = 100;
 				isRecovering = false;
+				gS.GetAudioSource().Stop();
+				
 			}
 		} else {
 			var fire = Input.GetAxis("Fire1");
@@ -24,12 +36,21 @@ public class Gun : MonoBehaviour {
 				WaterLevel -= ConsumeSpeed * Time.deltaTime;
 				if (WaterLevel > 0f) {
 					Particles.Play();
+					if(!gS.GetAudioSource().isPlaying && !gS.isPaused) {
+						gS.PlayShootSound();
+					}
+					else if(gS.isPaused) {
+						gS.Resume();
+					}
 				} else {
 					isRecovering = true;
 					Particles.Stop();
+					gS.GetAudioSource().Stop();
 				}
 			} else {
 				Particles.Stop();
+				gS.Pause();
+
 			}
 		}
 
