49b0a39615f402fb7ad4c9d860d73be376ecda57
Ed <eduardo_casillas@hotmail.com>
2019-12-01 01:46:02 -0600

    Added logo scene at start.

---
+++ Assets/Script/MainMenuLoader.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using Enums;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+[RequireComponent(typeof(VideoPlayer))]
+public class MainMenuLoader : MonoBehaviour {
+	private IEnumerator Start() {
+		var asyncLoad = SceneManager.LoadSceneAsync(Scenes.MainMenu.ToString());
+		asyncLoad.allowSceneActivation = false;
+
+		var videoPlayer = GetComponent<VideoPlayer>();
+
+		videoPlayer.Play();
+		yield return new WaitForSecondsRealtime((float)videoPlayer.clip.length);
+		
+		asyncLoad.allowSceneActivation = true;
+
+		// Wait until the asynchronous scene fully loads
+		while (!asyncLoad.isDone) {
+			yield return null;
+		}
+	}
+}
